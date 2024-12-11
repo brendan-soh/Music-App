ae666eebfcfae94958917d33128c09cb32f14d18
Brendan Soh <brendan.soh2@gmail.com>
2024-12-11 14:40:43 -0500

    Code update.

--- recommender/tests/__init__.py
+++ recommender/tests/__init__.py
@@ -13,16 +13,23 @@ class TestMusicRecommender(unittest.TestCase):
         self.assertEqual(len(recommendations), 5)
         self.assertNotIn(test_song_id, recommendations)
 
-    def test_song_details(self):
+    def test_invalid_song_id(self):
+        with self.assertRaises(ValueError):
+            self.recommender.get_recommendations(-1)
+
+    def test_get_song_features(self):
         test_song_id = self.recommender.df['id'].iloc[0]
-        song_details = self.recommender.get_song_details(test_song_id)
+        song_features = self.recommender.get_song_features(test_song_id)
 
-        self.assertIsNotNone(song_details)
-        self.assertEqual(song_details['id'], test_song_id)
-
-    def test_no_song_details(self):
-        song_details = self.recommender.get_song_details(-1)
-        self.assertIsNone(song_details)
+        # Check that all expected keys are present
+        expected_keys = [
+            'danceability', 'energy', 'key', 'loudness',
+            'speechiness', 'acousticness', 'instrumentalness',
+            'liveness', 'valence', 'tempo',
+            'track', 'artist'
+        ]
+        for key in expected_keys:
+            self.assertIn(key, song_features)
 
 if __name__ == '__main__':
     unittest.main()
